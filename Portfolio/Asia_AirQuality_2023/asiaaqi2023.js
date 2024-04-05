3c6ed4f1fc44b1b066608e7ad2414bd3d7b8e144
Tien Phuong Nguyen <ronnynguyen@Tiens-MacBook-Pro.local>
2024-04-05 15:58:34 +1100

    First Commit

---
+++ Portfolio/Asia_AirQuality_2023/asiaaqi2023.js
@@ -0,0 +1,221 @@
+var airquality = d3.json(
+	"Portfolio/Asia_AirQuality_2023/data/asiaaqi2023.json"
+);
+airquality
+	.then(function (data) {
+		ridgeLine(data);
+	})
+	.catch(function (error) {
+		// Handle any errors that occurred during the loading
+		console.error("Error loading data:", error);
+	});
+
+function ridgeLine(airquality) {
+	// Unload data
+	const months = [...new Set(Array.from(airquality.map((d) => d.Month)))];
+	const series = d3
+		.groups(airquality, (d) => d.Country)
+		.map(([name, values]) => {
+			const value = new Map(values.map((d) => [d.Month, d.AQI]));
+			return { name, values: months.map((d) => value.get(d)) };
+		});
+
+	// Set up color
+	var countries = [...new Set(airquality.map((d) => d.Country))];
+	var allMeans = [];
+	for (let c in countries) {
+		let key = countries[c];
+		let curr = airquality.filter((s) => s.Country == key);
+		let mean = d3.mean(curr.map((d) => d.AQI));
+		allMeans.push(mean);
+	}
+	allMeans.sort((a, b) => b - a);
+
+	// Calculate the minimum and maximum mean values
+	const minMean = d3.min(allMeans);
+	const maxMean = d3.max(allMeans);
+
+	// Normalize each mean value to the range [0, 1]
+	const normalizedMeans = allMeans.map((mean) => {
+		return ((mean - minMean) / (maxMean - minMean)) * 100;
+	});
+
+	var color = d3
+		.scaleSequential()
+		.domain([0, 100])
+		.interpolator(d3.interpolateCool);
+
+	var marginTop = 30,
+		marginBottom = 30,
+		marginLeft = 120,
+		marginRight = 120;
+	var width = d3.select(".bg").node().getBoundingClientRect().width;
+	var cell = 17;
+	var overlap = 15;
+	var height = series.length * cell;
+
+	// Create the scales
+	const x = d3
+		.scalePoint()
+		.domain(months)
+		.range([marginLeft, width - marginRight]);
+
+	const y = d3
+		.scalePoint()
+		.domain(series.map((d) => d.name))
+		.range([marginTop, height - marginBottom]);
+	const z = d3
+		.scaleLinear()
+		.domain([0, d3.max(series, (d) => d3.max(d.values))])
+		.nice()
+		.range([0, -overlap * y.step()]);
+
+	// Create the area
+	const area = d3
+		.area()
+		.curve(d3.curveBasis)
+		.defined((d) => !isNaN(d))
+		.x((d, i) => x(months[i]))
+		.y0(0)
+		.y1((d) => z(d));
+
+	const line = area.lineY1();
+
+	// Append the svg
+	var svg = d3
+		.select("#ridge-line")
+		.append("svg")
+		.attr("width", "100%")
+		.attr("height", height);
+
+	// Append the axes.
+	// x-axis
+	svg
+		.append("g")
+		.attr("transform", `translate(0,${height - marginBottom})`)
+		.call(d3.axisBottom(x).ticks(12).tickSizeOuter(0).tickPadding(5))
+		.call((g) => g.select(".domain").remove());
+	// y-axis
+	svg
+		.append("g")
+		.attr("transform", `translate(${marginLeft},0)`)
+		.call(d3.axisLeft(y).tickSize(0).tickPadding(4))
+		.call((g) => g.select(".domain").remove());
+
+	// Append a layer for each series.
+	const group = svg
+		.append("g")
+		.selectAll("g")
+		.data(series)
+		.join("g")
+		.attr("transform", (d) => `translate(0,${y(d.name)})`);
+
+	group
+		.append("path")
+		.attr("class", "area-path")
+		.attr("fill", function (d) {
+			let grp = d.name;
+			let index = countries.indexOf(grp);
+			let value = normalizedMeans[index];
+			return color(value);
+		}) // You can change the color as needed
+		.attr("fill-opacity", 0.4) // Adjust opacity as needed
+		.attr("d", (d) => area(d.values))
+		.append("title") // Add tooltip
+		.text(
+			(d) => `Country: ${d.name}, Average AQI: ${d3.mean(d.values).toFixed(2)}`
+		)
+		.attr("class", "tool-tips");
+	// Append lines for each series.
+	group
+		.append("path")
+		.attr("fill", "none")
+		.attr("stroke", function (d) {
+			let grp = d.name;
+			let index = countries.indexOf(grp);
+			let value = normalizedMeans[index];
+			return color(value);
+		}) // You can change the color as needed
+		.attr("stroke-width", 1) // Adjust stroke width as needed
+		.attr("d", (d) => line(d.values));
+
+	// Append color legend
+	var color = d3
+		.scaleSequential()
+		.domain([0, 100])
+		.interpolator(d3.interpolateCool);
+	const legend = svg
+		.append("g")
+		.attr("class", "legend")
+		.attr("transform", `translate(${width - marginRight / 1.2 + 10},${cell})`);
+
+	// Create color gradient for legend
+	const defs = legend.append("defs");
+	const linearGradient = defs
+		.append("linearGradient")
+		.attr("id", "color-gradient")
+		.attr("x1", "0%")
+		.attr("y1", "100%")
+		.attr("x2", "0%")
+		.attr("y2", "0%");
+	linearGradient
+		.append("stop")
+		.attr("offset", "0%")
+		.attr("stop-color", color(0));
+	linearGradient
+		.append("stop")
+		.attr("offset", "100%")
+		.attr("stop-color", color(100));
+
+	// Append color legend rectangle
+	legend
+		.append("rect")
+		.attr("rx", 2) // horizontal radius
+		.attr("ry", 2) // vertical radius
+		.attr("width", marginRight / 4)
+		.attr("height", height - marginBottom - cell)
+		.style("fill", "url(#color-gradient)");
+
+	var doS = height - marginBottom - cell;
+	// Append color legend axis
+	const legendScale = d3
+		.scaleOrdinal()
+		.domain([
+			d3.max(allMeans).toFixed(1),
+			55.4,
+			35.4,
+			12,
+			d3.min(allMeans).toFixed(1),
+		])
+		.range([
+			doS * ((80.3 - 9.3) / 71),
+			doS * ((55.4 - 9.3) / 71),
+			doS * ((35.4 - 9.3) / 71),
+			doS * ((12 - 9.3) / 71),
+			doS * ((9.3 - 9.3) / 71),
+		]);
+	const legendAxis = d3.axisRight(legendScale);
+	legend
+		.append("g")
+		.attr("transform", `translate(${marginRight / 4} , 0)`) // Adjust position
+		.call(legendAxis)
+		.call((g) => g.select(".domain").remove());
+}
+var scrollPos = 0;
+
+// Resize the chart after window resizing
+window.addEventListener("resize", function () {
+	// Store current scroll position
+	scrollPos = window.scrollY;
+
+	// Remove previous svg
+	d3.select("#ridge-line svg").remove();
+
+	// Redraw chart
+	airquality.then(function (data) {
+		ridgeLine(data);
+
+		// Reset scroll position
+		window.scrollTo(0, scrollPos);
+	});
+});
---
+++ Portfolio/Asia_AirQuality_2023/txt.js
@@ -0,0 +1,753 @@
+var airquality = [
+	{
+		Country: "Bangladesh",
+		Month: "Jan",
+		AQI: 175.5,
+	},
+	{
+		Country: "Bangladesh",
+		Month: "Feb",
+		AQI: 107.0,
+	},
+	{
+		Country: "Bangladesh",
+		Month: "Mar",
+		AQI: 82.5,
+	},
+	{
+		Country: "Bangladesh",
+		Month: "Apr",
+		AQI: 61.6,
+	},
+	{
+		Country: "Bangladesh",
+		Month: "May",
+		AQI: 49.2,
+	},
+	{
+		Country: "Bangladesh",
+		Month: "Jun",
+		AQI: 42.7,
+	},
+	{
+		Country: "Bangladesh",
+		Month: "Jul",
+		AQI: 23.6,
+	},
+	{
+		Country: "Bangladesh",
+		Month: "Aug",
+		AQI: 44.8,
+	},
+	{
+		Country: "Bangladesh",
+		Month: "Sep",
+		AQI: 39.0,
+	},
+	{
+		Country: "Bangladesh",
+		Month: "Oct",
+		AQI: 74.9,
+	},
+	{
+		Country: "Bangladesh",
+		Month: "Nov",
+		AQI: 101.6,
+	},
+	{
+		Country: "Bangladesh",
+		Month: "Dec",
+		AQI: 160.3,
+	},
+	{
+		Country: "Pakistan",
+		Month: "Jan",
+		AQI: 87.8333333333,
+	},
+	{
+		Country: "Pakistan",
+		Month: "Feb",
+		AQI: 64.9,
+	},
+	{
+		Country: "Pakistan",
+		Month: "Mar",
+		AQI: 43.0222222222,
+	},
+	{
+		Country: "Pakistan",
+		Month: "Apr",
+		AQI: 27.8222222222,
+	},
+	{
+		Country: "Pakistan",
+		Month: "May",
+		AQI: 28.2111111111,
+	},
+	{
+		Country: "Pakistan",
+		Month: "Jun",
+		AQI: 30.7777777778,
+	},
+	{
+		Country: "Pakistan",
+		Month: "Jul",
+		AQI: 28.9888888889,
+	},
+	{
+		Country: "Pakistan",
+		Month: "Aug",
+		AQI: 35.0666666667,
+	},
+	{
+		Country: "Pakistan",
+		Month: "Sep",
+		AQI: 41.9666666667,
+	},
+	{
+		Country: "Pakistan",
+		Month: "Oct",
+		AQI: 56.7666666667,
+	},
+	{
+		Country: "Pakistan",
+		Month: "Nov",
+		AQI: 111.5333333333,
+	},
+	{
+		Country: "Pakistan",
+		Month: "Dec",
+		AQI: 129.1222222222,
+	},
+	{
+		Country: "India",
+		Month: "Jan",
+		AQI: 79.3039823009,
+	},
+	{
+		Country: "India",
+		Month: "Feb",
+		AQI: 59.560619469,
+	},
+	{
+		Country: "India",
+		Month: "Mar",
+		AQI: 48.9991150442,
+	},
+	{
+		Country: "India",
+		Month: "Apr",
+		AQI: 43.0026548673,
+	},
+	{
+		Country: "India",
+		Month: "May",
+		AQI: 37.2115044248,
+	},
+	{
+		Country: "India",
+		Month: "Jun",
+		AQI: 30.0840707965,
+	},
+	{
+		Country: "India",
+		Month: "Jul",
+		AQI: 22.542920354,
+	},
+	{
+		Country: "India",
+		Month: "Aug",
+		AQI: 27.232300885,
+	},
+	{
+		Country: "India",
+		Month: "Sep",
+		AQI: 26.317699115,
+	},
+	{
+		Country: "India",
+		Month: "Oct",
+		AQI: 48.2256637168,
+	},
+	{
+		Country: "India",
+		Month: "Nov",
+		AQI: 77.8022123894,
+	},
+	{
+		Country: "India",
+		Month: "Dec",
+		AQI: 69.5451327434,
+	},
+	{
+		Country: "Tajikistan",
+		Month: "Jan",
+		AQI: 64.4,
+	},
+	{
+		Country: "Tajikistan",
+		Month: "Feb",
+		AQI: 38.5,
+	},
+	{
+		Country: "Tajikistan",
+		Month: "Mar",
+		AQI: 24.2,
+	},
+	{
+		Country: "Tajikistan",
+		Month: "Apr",
+		AQI: 38.4,
+	},
+	{
+		Country: "Tajikistan",
+		Month: "May",
+		AQI: 33.5,
+	},
+	{
+		Country: "Tajikistan",
+		Month: "Jun",
+		AQI: 35.5,
+	},
+	{
+		Country: "Tajikistan",
+		Month: "Jul",
+		AQI: 41.7,
+	},
+	{
+		Country: "Tajikistan",
+		Month: "Aug",
+		AQI: 33.1,
+	},
+	{
+		Country: "Tajikistan",
+		Month: "Sep",
+		AQI: 51.2,
+	},
+	{
+		Country: "Tajikistan",
+		Month: "Oct",
+		AQI: 44.7,
+	},
+	{
+		Country: "Tajikistan",
+		Month: "Nov",
+		AQI: 64.4,
+	},
+	{
+		Country: "Tajikistan",
+		Month: "Dec",
+		AQI: 92.0,
+	},
+	{
+		Country: "United Arab Emirates",
+		Month: "Jan",
+		AQI: 24.8666666667,
+	},
+	{
+		Country: "United Arab Emirates",
+		Month: "Feb",
+		AQI: 33.5,
+	},
+	{
+		Country: "United Arab Emirates",
+		Month: "Mar",
+		AQI: 41.0666666667,
+	},
+	{
+		Country: "United Arab Emirates",
+		Month: "Apr",
+		AQI: 37.5,
+	},
+	{
+		Country: "United Arab Emirates",
+		Month: "May",
+		AQI: 35.7333333333,
+	},
+	{
+		Country: "United Arab Emirates",
+		Month: "Jun",
+		AQI: 43.3,
+	},
+	{
+		Country: "United Arab Emirates",
+		Month: "Jul",
+		AQI: 59.3333333333,
+	},
+	{
+		Country: "United Arab Emirates",
+		Month: "Aug",
+		AQI: 63.7,
+	},
+	{
+		Country: "United Arab Emirates",
+		Month: "Sep",
+		AQI: 57.9666666667,
+	},
+	{
+		Country: "United Arab Emirates",
+		Month: "Oct",
+		AQI: 55.8333333333,
+	},
+	{
+		Country: "United Arab Emirates",
+		Month: "Nov",
+		AQI: 33.7333333333,
+	},
+	{
+		Country: "United Arab Emirates",
+		Month: "Dec",
+		AQI: 47.3,
+	},
+	{
+		Country: "Nepal",
+		Month: "Jan",
+		AQI: 97.7,
+	},
+	{
+		Country: "Nepal",
+		Month: "Feb",
+		AQI: 68.58,
+	},
+	{
+		Country: "Nepal",
+		Month: "Mar",
+		AQI: 69.72,
+	},
+	{
+		Country: "Nepal",
+		Month: "Apr",
+		AQI: 82.2,
+	},
+	{
+		Country: "Nepal",
+		Month: "May",
+		AQI: 40.2,
+	},
+	{
+		Country: "Nepal",
+		Month: "Jun",
+		AQI: 26.96,
+	},
+	{
+		Country: "Nepal",
+		Month: "Jul",
+		AQI: 16.2,
+	},
+	{
+		Country: "Nepal",
+		Month: "Aug",
+		AQI: 15.76,
+	},
+	{
+		Country: "Nepal",
+		Month: "Sep",
+		AQI: 18.06,
+	},
+	{
+		Country: "Nepal",
+		Month: "Oct",
+		AQI: 25.18,
+	},
+	{
+		Country: "Nepal",
+		Month: "Nov",
+		AQI: 39.94,
+	},
+	{
+		Country: "Nepal",
+		Month: "Dec",
+		AQI: 55.82,
+	},
+	{
+		Country: "Kuwait",
+		Month: "Jan",
+		AQI: 58.2,
+	},
+	{
+		Country: "Kuwait",
+		Month: "Feb",
+		AQI: 35.3,
+	},
+	{
+		Country: "Kuwait",
+		Month: "Mar",
+		AQI: 37.1,
+	},
+	{
+		Country: "Kuwait",
+		Month: "Apr",
+		AQI: 27.9,
+	},
+	{
+		Country: "Kuwait",
+		Month: "May",
+		AQI: 33.2,
+	},
+	{
+		Country: "Kuwait",
+		Month: "Jun",
+		AQI: 32.5,
+	},
+	{
+		Country: "Kuwait",
+		Month: "Jul",
+		AQI: 27.7,
+	},
+	{
+		Country: "Kuwait",
+		Month: "Aug",
+		AQI: 39.4,
+	},
+	{
+		Country: "Kuwait",
+		Month: "Sep",
+		AQI: 37.9,
+	},
+	{
+		Country: "Kuwait",
+		Month: "Oct",
+		AQI: 37.5,
+	},
+	{
+		Country: "Kuwait",
+		Month: "Nov",
+		AQI: 53.0,
+	},
+	{
+		Country: "Kuwait",
+		Month: "Dec",
+		AQI: 58.5,
+	},
+	{
+		Country: "Iraq",
+		Month: "Jan",
+		AQI: 59.15,
+	},
+	{
+		Country: "Iraq",
+		Month: "Feb",
+		AQI: 43.4,
+	},
+	{
+		Country: "Iraq",
+		Month: "Mar",
+		AQI: 39.4,
+	},
+	{
+		Country: "Iraq",
+		Month: "Apr",
+		AQI: 25.35,
+	},
+	{
+		Country: "Iraq",
+		Month: "May",
+		AQI: 37.15,
+	},
+	{
+		Country: "Iraq",
+		Month: "Jun",
+		AQI: 27.05,
+	},
+	{
+		Country: "Iraq",
+		Month: "Jul",
+		AQI: 23.15,
+	},
+	{
+		Country: "Iraq",
+		Month: "Aug",
+		AQI: 28.45,
+	},
+	{
+		Country: "Iraq",
+		Month: "Sep",
+		AQI: 30.35,
+	},
+	{
+		Country: "Iraq",
+		Month: "Oct",
+		AQI: 39.3,
+	},
+	{
+		Country: "Iraq",
+		Month: "Nov",
+		AQI: 54.05,
+	},
+	{
+		Country: "Iraq",
+		Month: "Dec",
+		AQI: 46.4,
+	},
+	{
+		Country: "Qatar",
+		Month: "Jan",
+		AQI: 43.0,
+	},
+	{
+		Country: "Qatar",
+		Month: "Feb",
+		AQI: 32.0,
+	},
+	{
+		Country: "Qatar",
+		Month: "Mar",
+		AQI: 30.7,
+	},
+	{
+		Country: "Qatar",
+		Month: "Apr",
+		AQI: 27.3,
+	},
+	{
+		Country: "Qatar",
+		Month: "May",
+		AQI: 27.1,
+	},
+	{
+		Country: "Qatar",
+		Month: "Jun",
+		AQI: 36.4,
+	},
+	{
+		Country: "Qatar",
+		Month: "Jul",
+		AQI: 36.4,
+	},
+	{
+		Country: "Qatar",
+		Month: "Aug",
+		AQI: 49.1,
+	},
+	{
+		Country: "Qatar",
+		Month: "Sep",
+		AQI: 42.8,
+	},
+	{
+		Country: "Qatar",
+		Month: "Oct",
+		AQI: 42.5,
+	},
+	{
+		Country: "Qatar",
+		Month: "Nov",
+		AQI: 35.8,
+	},
+	{
+		Country: "Qatar",
+		Month: "Dec",
+		AQI: 47.0,
+	},
+	{
+		Country: "Saudi Arabia",
+		Month: "Jan",
+		AQI: 42.8,
+	},
+	{
+		Country: "Saudi Arabia",
+		Month: "Feb",
+		AQI: 36.95,
+	},
+	{
+		Country: "Saudi Arabia",
+		Month: "Mar",
+		AQI: 31.4,
+	},
+	{
+		Country: "Saudi Arabia",
+		Month: "Apr",
+		AQI: 30.55,
+	},
+	{
+		Country: "Saudi Arabia",
+		Month: "May",
+		AQI: 39.1,
+	},
+	{
+		Country: "Saudi Arabia",
+		Month: "Jun",
+		AQI: 34.9,
+	},
+	{
+		Country: "Saudi Arabia",
+		Month: "Jul",
+		AQI: 27.45,
+	},
+	{
+		Country: "Saudi Arabia",
+		Month: "Aug",
+		AQI: 42.15,
+	},
+	{
+		Country: "Saudi Arabia",
+		Month: "Sep",
+		AQI: 44.05,
+	},
+	{
+		Country: "Saudi Arabia",
+		Month: "Oct",
+		AQI: 33.15,
+	},
+	{
+		Country: "Saudi Arabia",
+		Month: "Nov",
+		AQI: 38.7,
+	},
+	{
+		Country: "Saudi Arabia",
+		Month: "Dec",
+		AQI: 34.9,
+	},
+	{
+		Country: "Vietnam",
+		Month: "Jan",
+		AQI: 48.9363636364,
+	},
+	{
+		Country: "Vietnam",
+		Month: "Feb",
+		AQI: 41.5545454545,
+	},
+	{
+		Country: "Vietnam",
+		Month: "Mar",
+		AQI: 39.3545454545,
+	},
+	{
+		Country: "Vietnam",
+		Month: "Apr",
+		AQI: 33.9,
+	},
+	{
+		Country: "Vietnam",
+		Month: "May",
+		AQI: 28.0454545455,
+	},
+	{
+		Country: "Vietnam",
+		Month: "Jun",
+		AQI: 23.4181818182,
+	},
+	{
+		Country: "Vietnam",
+		Month: "Jul",
+		AQI: 24.7727272727,
+	},
+	{
+		Country: "Vietnam",
+		Month: "Aug",
+		AQI: 28.4181818182,
+	},
+	{
+		Country: "Vietnam",
+		Month: "Sep",
+		AQI: 30.9909090909,
+	},
+	{
+		Country: "Vietnam",
+		Month: "Oct",
+		AQI: 37.3090909091,
+	},
+	{
+		Country: "Vietnam",
+		Month: "Nov",
+		AQI: 48.1,
+	},
+	{
+		Country: "Vietnam",
+		Month: "Dec",
+		AQI: 39.5636363636,
+	},
+];
+
+ridgeLine(airquality);
+
+function ridgeLine(airquality) {
+	// Unload data
+	const months = Array.from(airquality.map((d) => d.Month));
+	const series = d3
+		.groups(airquality, (d) => d.Country)
+		.map(([name, values]) => {
+			const value = new Map(values.map((d) => [d.Month, d.AQI]));
+			return { name, values: months.map((d) => value.get(d)) };
+		});
+
+	var marginTop = 50,
+		marginBottom = 20,
+		marginLeft = 110,
+		marginRight = 0;
+	var width = d3.select(".bg").node().getBoundingClientRect().width;
+	var height = series.length * 20;
+	var overlap = 1;
+
+	// Create the scales.
+	const x = d3
+		.scaleBand()
+		.domain(months)
+		.range([marginLeft, width - marginRight]);
+
+	const y = d3
+		.scalePoint()
+		.domain(series.map((d) => d.name))
+		.range([marginTop, height - marginBottom]);
+	const z = d3
+		.scaleLinear()
+		.domain([0, d3.max(series, (d) => d3.max(d.values))])
+		.nice()
+		.range([0, -overlap * y.step()]);
+
+	// Create the area
+	const area = d3
+		.area()
+		.curve(d3.curveBasis)
+		.defined((d) => !isNaN(d))
+		.x((d, i) => x(months[i]))
+		.y0(0)
+		.y1((d) => z(d));
+
+	const line = area.lineY1();
+
+	// Append the svg
+	var svg = d3.select("#ridge-line").append("svg").attr("width", "100%");
+
+	// Append the axes.
+	svg.attr("height", height);
+	svg
+		.append("g")
+		.attr("transform", `translate(0,${height - marginBottom})`)
+		.call(
+			d3
+				.axisBottom(x)
+				.ticks(width / 80)
+				.tickSizeOuter(0)
+		);
+
+	svg
+		.append("g")
+		.attr("transform", `translate(${marginLeft},0)`)
+		.call(d3.axisLeft(y).tickSize(0).tickPadding(4))
+		.call((g) => g.select(".domain").remove());
+
+	// Append a layer for each series.
+	const group = svg
+		.append("g")
+		.selectAll("g")
+		.data(series)
+		.join("g")
+		.attr("transform", (d) => `translate(30,${y(d.name)})`);
+
+	group
+		.append("path")
+		.attr("class", "shading-area")
+		.attr("d", (d) => area(d.values));
+
+	group
+		.append("path")
+		.attr("class", "line")
+		.attr("fill", "none")
+		.attr("stroke", "black")
+		.attr("d", (d) => line(d.values));
+
+	console.log(series.map((d) => d.values));
+}
