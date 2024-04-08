fa17548ccee744cedcf0f16e6c12e60e45a15cdf
Ronny P Nguyen <phuong.nguyentien192@gmail.com>
2024-04-09 01:53:14 +1000

    new article svg

--- Portfolio/CryptoMarket_2024/cryptomarket2024.js
+++ Portfolio/CryptoMarket_2024/cryptomarket2024.js
@@ -1,4 +1,4 @@
-var loadData = d3.json("data/watchList.json");
+var loadData = d3.csv("data/watchList.csv");
 loadData
 	.then(function (processData) {
 		streamGraph(processData);
@@ -9,30 +9,61 @@ loadData
 	});
 
 function streamGraph(altcoin) {
-	var svg = d3.select("#stream-graph").attr("width", "100%");
+	var svg = d3.select("#stream-graph").append("svg").attr("width", "100%");
 	var svgDimension = svg.node().getBoundingClientRect();
 	var width = svgDimension.width;
 	var height = 600;
+	svg.attr("height", "600px");
 
 	var marginTop = 50,
 		marginBottom = 50,
-		marginRight = 0,
-		marginLeft = 0;
+		marginRight = 50,
+		marginLeft = 50;
 
-	const dates = [
-		...new Set(Array.from(altcoin.map((d) => d.date)).sort(d3.ascending)),
-	];
-
-	var x = d3.scaleTime().domain(d3.extent(dates)).range([0, width]);
-
-	var y = d3.scalePoint().domain([0, 5, 10000000000]).range([height, 0]);
-
-	svg
-		.append("g")
-		.attr("transform", `translate(${marginLeft}, ${marginBottom})`)
-		.call(d3.axisBottom(x).tickSize(-height * 0.7));
+	var keys = altcoin.columns.slice(1);
+	console.log(
+		d3.extent(altcoin, function (d) {
+			console.log(d.date);
+		})
+	);
+	// Add X axis
+	var x = d3
+		.scaleLinear()
+		.domain(
+			d3.extent(altcoin, function (d) {
+				return d.date;
+			})
+		)
+		.range([0, width - marginLeft - marginRight]);
 	svg
 		.append("g")
 		.attr("transform", `translate(${marginLeft}, ${height - marginBottom})`)
+		.call(d3.axisBottom(x).ticks(5));
+
+	// Add Y axis
+	var y = d3
+		.scaleLinear()
+		.domain([-100, 100])
+		.range([height - marginBottom - marginTop, 0]);
+	svg
+		.append("g")
+		.attr("transform", `translate(${marginLeft}, ${marginBottom})`)
 		.call(d3.axisLeft(y));
+
+	// color palette
+	var color = d3.scaleOrdinal().domain(keys).range(d3.schemecategory10);
+
+	var stackedData = d3.stack().offset(d3.stackOffsetSilhouette).keys(keys);
+
+	var area = d3
+		.area()
+		.x(function (d) {
+			return x(d.altcoin.date);
+		})
+		.y0(function (d) {
+			return y(d[0]);
+		})
+		.y1(function (d) {
+			return y(d[1]);
+		});
 }
