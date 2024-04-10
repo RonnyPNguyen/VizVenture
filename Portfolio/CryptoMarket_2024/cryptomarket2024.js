86865225f8cf9d348964ab12ad426bfbc89216ce
Ronny P Nguyen <phuong.nguyentien192@gmail.com>
2024-04-11 02:25:36 +1000

    11/04/2024 2am

--- Portfolio/CryptoMarket_2024/cryptomarket2024.js
+++ Portfolio/CryptoMarket_2024/cryptomarket2024.js
@@ -45,7 +45,7 @@ function streamGraph(altcoin, yAxisDomain) {
 	var height = 600;
 	svg.attr("height", height);
 
-	var marginTop = 80,
+	var marginTop = 40,
 		marginBottom = 80,
 		marginRight = 50,
 		marginLeft = 50;
@@ -55,21 +55,51 @@ function streamGraph(altcoin, yAxisDomain) {
 	var dates = altcoin.map((d) => new Date(d.date));
 	var duration = dates.length;
 
+	// Sumstat
+	var marketCap = {
+		bitcoin: 1375728580463.2524,
+		ethereum: 430639202176.3594,
+		binancecoin: 89155437215.42749,
+		solana: 84838706571.2534,
+		ripple: 34676270602.72719,
+		cardano: 23425863356.52765,
+		"avalanche-2": 21077240258.155663,
+		polkadot: 13028996507.219675,
+		chainlink: 11739367402.991226,
+		tron: 10591107950.99413,
+		dogecoin: 26140142029.04529,
+	};
+
+	var tickerAbb = {
+		bitcoin: "BTC",
+		ethereum: "ETH",
+		binancecoin: "BNB",
+		solana: "SOL",
+		ripple: "XRP",
+		cardano: "ADA",
+		"avalanche-2": "AVAX",
+		polkadot: "DOT",
+		chainlink: "LINK",
+		tron: "TRX",
+		dogecoin: "DOGE",
+	};
+
+	// Add color scale
 	const color = d3.scaleOrdinal().domain(keys).range([
-		"#e76f51", // Avalanche
-		"#f4a261", // Dogecoin
-		"#e9c46a", // Ripple
-		"#2a9d8f", // Shiba Inu
 		"#577590", // Solana
+		"#e9c46a", // Ripple
+		"#f4a261", // Dogecoin
+		"#e76f51", // Avalanche
+		"#2a9d8f", // Shiba Inu
 	]);
 
 	// Conditioning
 	if (reportInterval == "yearly") {
-		var yAxisDomain = [0, 4e12];
+		var yAxisDomain = [0, 1e13];
 	} else if (reportInterval == "monthly") {
-		var yAxisDomain = [0, 5e11];
+		var yAxisDomain = [0, 2e12];
 	} else if (reportInterval == "daily") {
-		var yAxisDomain = [0, 4e10];
+		var yAxisDomain = [0, 8e10];
 	}
 
 	// Add X axis
@@ -80,7 +110,12 @@ function streamGraph(altcoin, yAxisDomain) {
 	svg
 		.append("g")
 		.attr("transform", `translate(${0}, ${height - marginBottom})`)
-		.call(d3.axisBottom(x).tickSize(-(height - marginBottom - marginTop)))
+		.call(
+			d3
+				.axisBottom(x)
+				.tickSize(-(height - marginBottom - marginTop))
+				.tickPadding([40])
+		)
 		.select(".domain")
 		.remove();
 	svg.selectAll(".tick line").attr("stroke", "#b8b8b8");
@@ -100,11 +135,40 @@ function streamGraph(altcoin, yAxisDomain) {
 	var stackedData = d3.stack().offset(d3.stackOffsetSilhouette).keys(keys)(
 		altcoin
 	);
+	// Add tool tips
+	var Tooltip = svg
+		.append("text")
+		.attr("x", marginLeft)
+		.attr("y", marginTop - 10)
+		.style("opacity", 0)
+		.style("font-size", 16);
+	// Add 3 hover effect
+	var mouseover = function (d) {
+		Tooltip.style("opacity", 1);
+		d3.selectAll(".myArea").style("opacity", 0.2);
+		d3.select(this).style("stroke", "black").style("opacity", 1);
+	};
+	var mousemove = function (i, d) {
+		var tickerName = d.key;
+		Tooltip.text(
+			`${tickerName} (${tickerAbb[tickerName]}) - market cap: $${(
+				marketCap[tickerName] / 1e9
+			)
+				.toFixed(1)
+				.toLocaleString()} trillion`
+		);
+	};
+	var mouseleave = function (d) {
+		Tooltip.style("opacity", 0);
+		d3.selectAll(".myArea").style("opacity", 1).style("stroke", "none");
+	};
+
 	svg
 		.selectAll("mylayers")
 		.data(stackedData)
 		.enter()
 		.append("path")
+		.attr("class", "myArea")
 		.style("fill", function (d) {
 			return color(d.key);
 		})
@@ -122,7 +186,10 @@ function streamGraph(altcoin, yAxisDomain) {
 				.y1(function (d) {
 					return y(d[1]);
 				})
-		);
+		)
+		.on("mouseover", mouseover)
+		.on("mousemove", mousemove)
+		.on("mouseleave", mouseleave);
 }
 
 // Resize the chart after window resizing
